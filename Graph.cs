7081c7a9f9f27c9dc03933baeea9315b8482bc14
Karol Piasnik <piasnik.karol@outlook.com>
2017-06-07 20:09:24 +0200

    Adden class graph with constructor

---
+++ Graph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Graph
+    {
+        int numberOfVertices;
+        bool[,] adjacency;
+
+        Graph(int numberOfVertices)
+        {
+            this.numberOfVertices = numberOfVertices;
+            adjacency = new bool[numberOfVertices, numberOfVertices];
+        }
+
+    }
+}
