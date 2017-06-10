f562f4ef29a6765cce8f3d5d802a283e31e84f53
Karol Piasnik <piasnik.karol@outlook.com>
2017-06-10 17:57:25 +0200

    implemented greedy alghorithm and starded from file constructor

--- Graph.cs
+++ Graph.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApplication1
 {
@@ -11,19 +12,85 @@ namespace ConsoleApplication1
         int numberOfVertices;
         bool[,] adjacency;
 
-        Graph(int numberOfVertices)
+        public Graph(int numberOfVertices)
         {
             this.numberOfVertices = numberOfVertices;
             adjacency = new bool[numberOfVertices, numberOfVertices];
+            for (int i = 0; i < numberOfVertices; ++i)
+                for (int j = 0; j < numberOfVertices; ++j)
+                    adjacency[i, j] = false;
+        }
+
+        public Graph(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            numberOfVertices = int.Parse(lines[0]);
+
+            if (lines.Length == numberOfVertices + 1)
+            {
+                for (int i = 0; i < numberOfVertices; ++i)
+                    for (int j = 0; j < numberOfVertices; ++j)
+                    {
+
+                    }
+            }
+            else
+            {
+                Console.WriteLine("xxx");
+            }
         }
 
         public void addEdge(int v1, int v2)
         {
             adjacency[v1, v2] = true;
+            adjacency[v2, v1] = true;
         }
 
         // Prints greedy coloring of the vertices
         public void greedyColoring()
-        { }
+        {
+            int[] result = new int[numberOfVertices];
+            // Assign the first color to first vertex
+            result[0] = 0;
+
+            // Initialize remaining V-1 vertices as unassigned
+            for (int u = 1; u < numberOfVertices; u++)
+                result[u] = -1;  // no color is assigned to u
+
+            // A temporary array to store the available color. True
+            // value of available[color] would mean that the color color is
+            // assigned to one of its adjacent vertices
+            bool[] available = new bool[numberOfVertices];
+            for (int color = 0; color < numberOfVertices; ++color)
+                available[color] = false;
+            // Assign color to remaining V-1 vertices
+            for (int u = 1; u < numberOfVertices; u++)
+            {
+                // Process all adjacent vertices and flag their color
+                // as unavailable
+                for (int i = 0; i < numberOfVertices; ++i)
+                    if (adjacency[u, i] == true)
+                        if (result[i] != -1)
+                        available[result[i]] = true;
+
+                // Find the first available color
+                int cr;
+                for (cr = 0; cr < numberOfVertices; cr++)
+                    if (available[cr] == false)
+                        break;
+
+                result[u] = cr; // Assign the found color
+
+                // Reset the values back to false for the next iteration
+                for (int i = 0; i < numberOfVertices; ++i)
+                    if(adjacency[u,i] == true)
+                        if (result[i] != -1)
+                            available[result[i]] = false;
+            }
+
+            // print the result
+            for (int u = 0; u < numberOfVertices; u++)
+                Console.WriteLine("Vertex " + u + "---> Color" + result[u] ); 
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Graph g1 = new Graph(5);
+
+            g1.addEdge(0, 1);
+            g1.addEdge(0, 2);
+            g1.addEdge(1, 2);
+            g1.addEdge(1, 3);
+            g1.addEdge(2, 3);
+            g1.addEdge(3, 4);
+            g1.greedyColoring();
         }
     }
 }
